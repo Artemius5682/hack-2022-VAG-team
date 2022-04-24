eb582367aa9ec84373c0ab492ec9ecf0d3a5bf1b
Artemius5682 <63748125+Artemius5682@users.noreply.github.com>
2022-04-24 05:47:09 +0300

    Добавлен проект
    Добавлен проект

---
+++ view/footer.php
@@ -0,0 +1,27 @@
+
+   <footer class="d-flex align-items-center" id="footer">
+      <div class="container text-center h-50">
+         <div class="row d-flex justify-content-between h-100 transform">
+            <div class="col-sm-4 d-flex flex-column justify-content-between h-100">
+               <h5>Navigation</h5>
+               <a href="">О нас</a>
+               <a href="">Каталог</a>
+               <a href="">Личные данные</a>
+            </div>
+            <div class="col-sm-4 d-flex flex-column justify-content-between h-100">
+               <h5>Follow us</h5>
+               <a href=""><i class="bi bi-bootstrap-fill"></i></a>
+               <a href=""><i class="bi bi-instagram"></i></a>
+               <a href=""><i class="bi bi-github"></i></a>
+            </div>
+            <div class="col-sm-4 d-flex flex-column justify-content-between h-100">
+               <h5>Our Location</h5>
+               <a href="">Lorem1</a>
+               <a href="">Lorem2</a>
+               <a href="">Lorem3</a>
+            </div>
+         </div>
+         <hr>
+         <p>Copyright &copy; 2022 Agrotime. Designed by <span><a href="https://vk.com/slickshoot666" target="_blank">VAG-team</a></span></p>
+      </div>
+   </footer>
